db517fd2a73c9289d821e674058f5d325076564d
yyc <1536376956@qq.com>
2023-03-17 22:55:22 +0800

    更新添加文章分类逻辑

--- src/router_handler/artcate.js
+++ src/router_handler/artcate.js
@@ -20,9 +20,18 @@ exports.getArtCateList = async (req, res) => {
 exports.addCates = async (req, res) => {
     const sql = 'select * from ev_article_cate where name=? or alias=?'
     const sqlStr = 'insert into ev_article_cate set ?'
+    const resetSql = 'update ev_article_cate set is_delete=0 where id=?'
     try {
         let result = await query(sql, [req.body.name, req.body.alias])
-        if (result.length) return res.cc('分类名称或别名被占用，请更换后重试！')
+        if (result.length) {
+            // 添加新文章时，可能查找到的之前删除的文章，将他恢复即可
+            if (result[0].is_delete == 1) {
+                let response = await query(resetSql, result[0].id)
+                if (response.affectedRows !== 1) return res.cc('新增文章分类失败！')
+                return res.cc('新增文章分类成功！', 0)
+            }
+            return res.cc('分类名称或别名被占用，请更换后重试！')
+        }
         let results = await query(sqlStr, req.body)
         if (results.affectedRows !== 1) return res.cc('新增文章分类失败！')
         res.cc('新增文章分类成功！', 0)
